7d400835765f602ab16e5fb9f316a8679642a499
Carlos Godoi <cgodoi@Carlos-MacBook-Pro.local>
2023-05-18 19:29:28 -0300

    android: working

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.scrollView}>
+        {Array.from({ length: 15 }).fill('').map((_, index) => (
+          <TextInput
+            key={index}
+            style={styles.input}
+            placeholder="I'm a TextInput"
+          />
+        ))}
+
+        {/* <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity> */}
+      </ScrollView>
+
+      <TouchableOpacity style={styles.absoluteButton}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
+
+      <View style={styles.spacer} />
     </View>
   );
 }
 
+const BUTTON_HEIGHT = 60;
+const BUTTON_MARGIN_BOTTOM = Platform.OS === 'ios' ? 20 : undefined;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+  },
+  scrollView: {
+    flex: 1,
+    width: '100%',
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: 6,
+    padding: 12,
+    marginBottom: 12,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 6,
+  },
+  absoluteButton: {
+    height: BUTTON_HEIGHT,
+    backgroundColor: 'blue',
+    padding: 20,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    marginBottom: BUTTON_MARGIN_BOTTOM,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 16,
+  },
+  spacer: {
+    height: BUTTON_HEIGHT,
   },
 });
